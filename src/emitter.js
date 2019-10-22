a09bcbd414e5c9a9254a7f50a9b3c278d1c27a51
William Kasel <wkasel@gmail.com>
2019-10-22 02:57:40 -0700

    fixes JFusco/es6-event-emitter#3

--- src/emitter.js
+++ src/emitter.js
@@ -1,100 +1,100 @@
 const emitter = new WeakMap();
 
 class Emitter {
-	constructor(){
-		emitter.set(this, {
-			events: {}
-		});
+  constructor() {
+    emitter.set(this, {
+      events: {}
+    });
 
-		this.eventLength = 0;
-	}
+    this.eventLength = 0;
+  }
 
-	on(event, cb, once = false){
-		if(typeof cb === 'undefined') {
-			throw new Error('You must provide a callback method.');
-		}
+  on(event, cb, once = false) {
+    if (typeof cb === 'undefined') {
+      throw new Error('You must provide a callback method.');
+    }
 
-		if(typeof cb !== 'function') {
-			throw new TypeError('Listener must be a function');
-		}
+    if (typeof cb !== 'function') {
+      throw new TypeError('Listener must be a function');
+    }
 
-		this.events[event] = this.events[event] || [];
-		this.events[event].push({
+    this.events[event] = this.events[event] || [];
+    this.events[event].push({
       cb,
       once
     });
 
-		this.eventLength++;
+    this.eventLength++;
 
-		return this;
-	}
+    return this;
+  }
 
-	off(event, cb){
-		if(typeof cb === 'undefined') {
-			throw new Error('You must provide a callback method.');
-		}
+  off(event, cb) {
+    if (typeof cb === 'undefined') {
+      throw new Error('You must provide a callback method.');
+    }
 
-		if(typeof cb !== 'function') {
-			throw new TypeError('Listener must be a function');
-		}
+    if (typeof cb !== 'function') {
+      throw new TypeError('Listener must be a function');
+    }
 
-		if(typeof this.events[event] === 'undefined'){
-			throw new Error(`Event not found - the event you provided is: ${event}`);
-		}
+    if (typeof this.events[event] === 'undefined') {
+      throw new Error(`Event not found - the event you provided is: ${event}`);
+    }
 
-		const listeners = this.events[event];
+    const listeners = this.events[event];
 
-		listeners.forEach((v, i) => {
-			if(v.cb === cb) {
-				listeners.splice(i, 1);
-			}
-		});
+    listeners.forEach((v, i) => {
+      if (v.cb === cb) {
+        listeners.splice(i, 1);
+      }
+    });
 
-		if(listeners.length === 0){
-			delete this.events[event];
+    if (listeners.length === 0) {
+      delete this.events[event];
 
-			this.eventLength--;
-		}
+      this.eventLength--;
+    }
 
-		return this;
-	}
+    return this;
+  }
 
-	trigger(event, ...args){
-		if(typeof event === 'undefined'){
-			throw new Error('You must provide an event to trigger.');
-		}
+  trigger(event, ...args) {
+    if (typeof event === 'undefined') {
+      throw new Error('You must provide an event to trigger.');
+    }
 
-		const listeners = this.events[event];
+    const listeners = this.events[event];
     const onceListeners = [];
 
-		if(typeof listeners !== 'undefined') {
-			listeners.forEach((v, k) => {
+    if (typeof listeners !== 'undefined') {
+      listeners.forEach((v, k) => {
         v.cb.apply(this, args);
 
-        if(v.once) onceListeners.unshift(k);
+        if (v.once) onceListeners.unshift(k);
 
         onceListeners.forEach((v, k) => {
           listeners.splice(k, 1);
         });
-			});
-		}
+      });
+    }
 
-		return this;
-	}
+    return this;
+  }
 
-  once(event, cb){
+  once(event, cb) {
     this.on(event, cb, true);
   }
 
-  destroy(){
+  destroy() {
     emitter.delete(this);
 
     this.eventLength = 0;
   }
 
-	get events(){
-		return emitter.get(this).events;
-	}
+  get events() {
+    return emitter.get(this).events;
+  }
 }
 
 export default Emitter;
